279b284c9558a2dd10ddf1bae03c218bf898ea46
catherinecunningham <cfunk512@gmail.com>
2022-11-09 17:00:06 -0800

    more inquirer questions for adding dept and role

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const db = mySQL.createConnection({
     host: "localhost"
 })
 
+db.connect(err => {
+    if (err) throw err;
+    else { start() }
+})
+
 function start() {
     inquirer.prompt([
         {
@@ -26,11 +31,62 @@ function start() {
             ]
         }
     ])
-    .then(input => {
-        if(input.todo == "View All Departments") {
-            db.query(`select * from department`, (err, res) => {
-                console.table(res)
+        .then(input => {
+            if (input.todo == "View All Departments") {
+                db.query(`select * from department`, (err, res) => {
+                    console.table(res)
+                    start()
+                })
+            }
+            else if (input.todo == "View All Roles") {
+                db.query(`select * from role`, (err, res) => {
+                    console.table(res)
+                    start()
+                })
+            }
+            else if (input.todo == "View All Employees") {
+                    db.query(`select * from employee`, (err, res) => {
+                        console.table(res)
+                        start()
+                    })
+            }
+            else if (input.todo == "Add Department") {
+                inquirer.prompt([
+                    {
+                        name: "deptname",
+                        message: "What is the name of the department?",
+                        type: "input"
+                    }
+
+                ])
+                .then(deptdata => {
+                    db.query(`insert into department(name) values("${deptdata.deptname}")`, (err, res) => {
+                        console.table(res)
+                        start()
+                    })
+                })
+            }
+            else if (input.todo == "Add Role") {
+                inquirer.prompt([
+                    {
+                        name: "rolename",
+                        message: "What role would you like to add?",
+                        type: "input"
+                    },
+                    {
+                        name: "rolesalary",
+                        message: "What salary is this position?",
+                        type: "input"
+                    },
+                    {
+                        name: "deptid",
+                        message: "What is the department ID for this role?",
+                        type: "input"
+                    }
+                ])
+                .then(roledata => {
+                    db.query(`insert into role(title, salary, department_id) values("${roledata.rolename}", ${roledata.rolesalary}, ${roledata.deptid});`)
+                })
+            }
             })
-        }
-    })
 }
